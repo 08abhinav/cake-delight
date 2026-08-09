1b6c9fe49e0749923cbf7e1e5911b820aea27e10
08abhinav <abhinavnegi101@gmail.com>
2026-08-09 16:00:31 +0530

    update: remove auth checking from gateway by checking in each service

--- cakeCatalog/middleware/auth.js
+++ cakeCatalog/middleware/auth.js
@@ -1,15 +1,30 @@
+import "dotenv/config";
 import jwt from "jsonwebtoken";
 
-export function authorizedSeller(){
-    return (req, res, next)=>{
-        try{
-            const role = req.headers['x-user-role']
-            if(role !== "seller"){
-                return res.status(403).json({success: false, message: "Permission denied"})
+export function authorizedSeller() {
+    return (req, res, next) => {
+        try {
+            const token = req.cookies?.token
+            if (!token) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized"
+                });
             }
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            if (decoded.role !== "seller") {
+                return res.status(403).json({
+                    success: false,
+                    message: "Permission denied"
+                });
+            }
+
+            req.user = decoded;
             next();
-        }catch(err){
+        } catch (err) {
             next(err);
         }
-    }
+    };
 }
