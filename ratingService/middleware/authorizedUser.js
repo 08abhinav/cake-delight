2d10edb401cec171b335528e392ad125dab46d26
08abhinav <abhinavnegi101@gmail.com>
2026-08-10 12:43:54 +0530

    validator and middlware

---
+++ ratingService/middleware/authorizedUser.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+
+export function authorizedUser() {
+    return (req, res, next) => {
+        try {
+            const token = req.cookies?.token
+            if (!token) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized"
+                });
+            }
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = decoded;
+            next();
+        } catch (err) {
+            next(err);
+        }
+    };
+}
